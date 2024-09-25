2b656e84ab2287b837fd508f2add9b20cb166fb2
Faisal8102 <faisaldar0001@gmail.com>
2024-09-25 23:01:52 +0530

    blog_post is working now we need to make some changes in this section

--- frontendapp/views.py
+++ frontendapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from backendapp.models import Project, Portfolio, Card
+from backendapp.models import Project, Portfolio, Card,Blog
 from django.views import View
 from backendapp.models import PostDetail
 # Create your views here.
@@ -13,12 +13,11 @@ def about(request):
 
 def blogs(request):
     # Fetch all blog posts
-    posts = Post.objects.all()
-    return render(request, 'pages/blogs.html', {'posts': posts})
+    blogs = Blog.objects.all()
+    return render(request, 'pages/blogs.html', {'blogs': blogs})
 
-def blog_post(request, post_id):
-    # Fetch a specific blog post by ID
-    post = get_object_or_404(PostDetail, id=post_id)
+def blog_post(request, id):
+    post = get_object_or_404(PostDetail, id=id) # or whatever filtering is necessary
     return render(request, 'pages/blog_post.html', {'post': post})
 
 
